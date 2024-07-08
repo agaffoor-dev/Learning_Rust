39babae79db87df64cd043b5a30cffd65e331fc4
agaffoor-dev <174390644+agaffoor-dev@users.noreply.github.com>
2024-07-08 16:27:55 -0400

    Added finishing touches as per the Rust documentation
    Added a loop and error checking for user input (added additional prompts on top of example)

--- guess_the_number_4/src/main.rs
+++ guess_the_number_4/src/main.rs
@@ -6,16 +6,25 @@ fn main() {
     println!("This is a guess the number game!");
     let secret_number = rand::thread_rng().gen_range(1..=10);
     println!("Guess a number in the range of 1 to 10");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Error reading string");
-    let guess:u16 = guess.trim().parse().expect("Not an integer");
-    println!("You guessed {}", guess);
-    println!("The secret number was {}", secret_number);//print secret number here
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win!"),
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error reading string");
+        let guess:u16 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Type an integer please.");
+                continue;
+            }
+        };
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
